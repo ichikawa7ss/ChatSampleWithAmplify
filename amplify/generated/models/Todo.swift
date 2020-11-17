85f4f46b77b12ba1818f7c3857a8b483eb0c5e0e
ichikawa7ss <i2s7.chacha@gmail.com>
2020-11-17 18:15:57 +0900

    Refactor.

--- amplify/generated/models/Todo.swift
+++ amplify/generated/models/Todo.swift
@@ -3,15 +3,15 @@ import Amplify
 import Foundation
 
 public struct Todo: Model {
-  public let id: String
-  public var name: String
-  public var description: String?
-  
-  public init(id: String = UUID().uuidString,
-      name: String,
-      description: String? = nil) {
-      self.id = id
-      self.name = name
-      self.description = description
-  }
+    public let id: String
+    public var name: String
+    public var description: String?
+    
+    public init(id: String = UUID().uuidString,
+                name: String,
+                description: String? = nil) {
+        self.id = id
+        self.name = name
+        self.description = description
+    }
 }
